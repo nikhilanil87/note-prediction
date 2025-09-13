c14d9c45368328722166d4fabdd161cacaac830d
nikhilanil87 <nikhilanil8721@gmail.com>
2025-09-13 15:32:24 +0530

    notes are fine

--- note_recognition.py
+++ note_recognition.py
@@ -1,103 +1,77 @@
 import argparse
-
 from pydub import AudioSegment
-import pydub.scipy_effects
 import numpy as np
-import scipy
+import scipy.signal
 import matplotlib.pyplot as plt
 
 from utils import (
     frequency_spectrum,
     calculate_distance,
-    classify_note_attempt_1,
-    classify_note_attempt_2,
     classify_note_attempt_3,
 )
 
 
 def main(file, note_file=None, note_starts_file=None, plot_starts=False, plot_fft_indices=[]):
-    # If a note file and/or actual start times are supplied read them in
+    # Load actual notes / starts if provided
     actual_starts = []
     if note_starts_file:
         with open(note_starts_file) as f:
-            for line in f:
-                actual_starts.append(float(line.strip()))
+            actual_starts = [float(line.strip()) for line in f]
 
     actual_notes = []
     if note_file:
         with open(note_file) as f:
-            for line in f:
-                actual_notes.append(line.strip())
+            actual_notes = [line.strip() for line in f]
 
     song = AudioSegment.from_file(file)
-    song = song.high_pass_filter(80, order=4)
+    song = song.high_pass_filter(80)  # Remove low-frequency noise
 
+    # Detect note starts
     starts = predict_note_starts(song, plot_starts, actual_starts)
 
+    # Predict notes
     predicted_notes = predict_notes(song, starts, actual_notes, plot_fft_indices)
 
-    print("")
+    print("\n--- RESULTS ---")
     if actual_notes:
-        print("Actual Notes")
+        print("Actual Notes:")
         print(actual_notes)
-    print("Predicted Notes")
+    print("Predicted Notes:")
     print(predicted_notes)
 
     if actual_notes:
         lev_distance = calculate_distance(predicted_notes, actual_notes)
-        print("Levenshtein distance: {}/{}".format(lev_distance, len(actual_notes)))
+        print(f"Levenshtein distance: {lev_distance}/{len(actual_notes)}")
 
 
-# Very simple implementation, just requires a minimum volume and looks for left edges by
-# comparing with the prior sample, also requires a minimum distance between starts
-# Future improvements could include smoothing and/or comparing multiple samples
-#
-# song: pydub.AudioSegment
-# plot: bool, whether to show a plot of start times
-# actual_starts: []float, time into song of each actual note start (seconds)
-#
-# Returns perdicted starts in ms
-def predict_note_starts(song, plot, actual_starts):
-    # Size of segments to break song into for volume calculations
+def predict_note_starts(song, plot=False, actual_starts=[]):
     SEGMENT_MS = 50
-    # Minimum volume necessary to be considered a note
-    VOLUME_THRESHOLD = -35
-    # The increase from one sample to the next required to be considered a note
-    EDGE_THRESHOLD = 5
-    # Throw out any additional notes found in this window
     MIN_MS_BETWEEN = 100
 
-    # Filter out lower frequencies to reduce noise
-    song = song.high_pass_filter(80, order=4)
-    # dBFS is decibels relative to the maximum possible loudness
-    volume = [segment.dBFS for segment in song[::SEGMENT_MS]]
+    # Use RMS for adaptive thresholding
+    volume = np.array([segment.rms for segment in song[::SEGMENT_MS]])
+    threshold = np.mean(volume) + 0.5 * np.std(volume)
 
     predicted_starts = []
     for i in range(1, len(volume)):
-        if volume[i] > VOLUME_THRESHOLD and volume[i] - volume[i - 1] > EDGE_THRESHOLD:
+        if volume[i] > threshold and volume[i] - volume[i - 1] > 0:
             ms = i * SEGMENT_MS
-            # Ignore any too close together
             if len(predicted_starts) == 0 or ms - predicted_starts[-1] >= MIN_MS_BETWEEN:
                 predicted_starts.append(ms)
 
-    # If actual note start times are provided print a comparison
     if len(actual_starts) > 0:
-        print("Approximate actual note start times ({})".format(len(actual_starts)))
-        print(" ".join(["{:5.2f}".format(s) for s in actual_starts]))
-        print("Predicted note start times ({})".format(len(predicted_starts)))
-        print(" ".join(["{:5.2f}".format(ms / 1000) for ms in predicted_starts]))
+        print(f"Actual starts ({len(actual_starts)}): {['{:.2f}'.format(s) for s in actual_starts]}")
+        print(f"Predicted starts ({len(predicted_starts)}): {['{:.2f}'.format(ms / 1000) for ms in predicted_starts]}")
 
-    # Plot the volume over time (sec)
     if plot:
         x_axis = np.arange(len(volume)) * (SEGMENT_MS / 1000)
         plt.plot(x_axis, volume)
-
-        # Add vertical lines for predicted note starts and actual note starts
         for s in actual_starts:
-            plt.axvline(x=s, color="r", linewidth=0.5, linestyle="-")
+            plt.axvline(x=s, color="r", linewidth=0.5)
         for ms in predicted_starts:
-            plt.axvline(x=(ms / 1000), color="g", linewidth=0.5, linestyle=":")
-
+            plt.axvline(x=ms / 1000, color="g", linewidth=0.5)
+        plt.xlabel("Time (s)")
+        plt.ylabel("RMS volume")
         plt.show()
 
     return predicted_starts
@@ -106,61 +80,55 @@ def predict_note_starts(song, plot, actual_starts):
 def predict_notes(song, starts, actual_notes, plot_fft_indices):
     predicted_notes = []
     for i, start in enumerate(starts):
-        sample_from = start + 50
-        sample_to = start + 550
+        sample_from = start
+        sample_to = start + 500  # 500 ms window
         if i < len(starts) - 1:
             sample_to = min(starts[i + 1], sample_to)
+
         segment = song[sample_from:sample_to]
         freqs, freq_magnitudes = frequency_spectrum(segment)
 
-        predicted = classify_note_attempt_3(freqs, freq_magnitudes)
+        # Classify note
+        try:
+            predicted = classify_note_attempt_3(freqs, freq_magnitudes)
+        except IndexError:
+            predicted = "U"
         predicted_notes.append(predicted or "U")
 
-        # Print general info
-        print("")
-        print("Note: {}".format(i))
+        # Info output
+        print(f"\nNote {i + 1}: Predicted={predicted}")
         if i < len(actual_notes):
-            print("Predicted: {} Actual: {}".format(predicted, actual_notes[i]))
-        else:
-            print("Predicted: {}".format(predicted))
-        print("Predicted start: {}".format(start))
-        length = sample_to - sample_from
-        print("Sampled from {} to {} ({} ms)".format(sample_from, sample_to, length))
-        print("Frequency sample period: {}hz".format(freqs[1]))
+            print(f"Actual={actual_notes[i]}")
+        print(f"Start={start} ms, Window={sample_from}-{sample_to} ms")
 
-        # Print peak info
-        peak_indicies, props = scipy.signal.find_peaks(freq_magnitudes, height=0.015)
-        print("Peaks of more than 1.5 percent of total frequency contribution:")
-        for j, peak in enumerate(peak_indicies):
-            freq = freqs[peak]
-            magnitude = props["peak_heights"][j]
-            print("{:.1f}hz with magnitude {:.3f}".format(freq, magnitude))
+        # Show peaks
+        peaks, props = scipy.signal.find_peaks(freq_magnitudes, height=0.015)
+        for j, p in enumerate(peaks):
+            print(f"Peak {j + 1}: {freqs[p]:.1f} Hz, magnitude {props['peak_heights'][j]:.3f}")
 
         if i in plot_fft_indices:
-            plt.plot(freqs, freq_magnitudes, "b")
-            plt.xlabel("Freq (Hz)")
-            plt.ylabel("|X(freq)|")
+            plt.plot(freqs, freq_magnitudes)
+            plt.xlabel("Frequency (Hz)")
+            plt.ylabel("Normalized magnitude")
+            plt.title(f"FFT for note {i + 1}")
             plt.show()
+
     return predicted_notes
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("file")
-    parser.add_argument("--note-file", type=str)
-    parser.add_argument("--note-starts-file", type=str)
-    parser.add_argument("--plot-starts", action="store_true")
-    parser.add_argument(
-        "--plot-fft-index",
-        type=int,
-        nargs="*",
-        help="Index of detected note to plot graph of FFT for",
-    )
+    parser.add_argument("file", help="Input audio file (wav, mp3, etc.)")
+    parser.add_argument("--note-file", type=str, help="Ground truth note labels")
+    parser.add_argument("--note-starts-file", type=str, help="Ground truth note start times")
+    parser.add_argument("--plot-starts", action="store_true", help="Plot predicted vs actual starts")
+    parser.add_argument("--plot-fft-index", type=int, nargs="*", help="Plot FFT for specific note indices")
     args = parser.parse_args()
+
     main(
         args.file,
         note_file=args.note_file,
         note_starts_file=args.note_starts_file,
         plot_starts=args.plot_starts,
-        plot_fft_indices=(args.plot_fft_index or []),
+        plot_fft_indices=args.plot_fft_index or [],
     )
--- utils.py
+++ utils.py
@@ -2,137 +2,124 @@ import array
 from collections import Counter
 
 import numpy as np
-import scipy
+from scipy.fft import fft
 from pydub.utils import get_array_type
 from Levenshtein import distance
 
+# Note frequencies (A4 = 440 Hz)
 NOTES = {
-    "A": 440,
-    "A#": 466.1637615180899,
-    "B": 493.8833012561241,
-    "C": 523.2511306011972,
-    "C#": 554.3652619537442,
-    "D": 587.3295358348151,
-    "D#": 622.2539674441618,
-    "E": 659.2551138257398,
-    "F": 698.4564628660078,
-    "F#": 739.9888454232688,
-    "G": 783.9908719634985,
-    "G#": 830.6093951598903,
+    "A": 440.00,
+    "A#": 466.16,
+    "B": 493.88,
+    "C": 523.25,
+    "C#": 554.37,
+    "D": 587.33,
+    "D#": 622.25,
+    "E": 659.26,
+    "F": 698.46,
+    "F#": 739.99,
+    "G": 783.99,
+    "G#": 830.61,
 }
-
 
 def frequency_spectrum(sample, max_frequency=800):
     """
-    Derive frequency spectrum of a signal pydub.AudioSample
-    Returns an array of frequencies and an array of how prevelant that frequency is in the sample
+    Derive frequency spectrum of a pydub.AudioSegment.
+    Returns frequencies and normalized magnitudes.
     """
-    # Convert pydub.AudioSample to raw audio data
-    # Copied from Jiaaro's answer on https://stackoverflow.com/questions/32373996/pydub-raw-audio-data
     bit_depth = sample.sample_width * 8
     array_type = get_array_type(bit_depth)
     raw_audio_data = array.array(array_type, sample._data)
     n = len(raw_audio_data)
 
-    # Compute FFT and frequency value for each index in FFT array
-    # Inspired by Reveille's answer on https://stackoverflow.com/questions/53308674/audio-frequencies-in-python
-    freq_array = np.arange(n) * (float(sample.frame_rate) / n)  # two sides frequency range
-    freq_array = freq_array[: (n // 2)]  # one side frequency range
+    raw_audio_data = np.array(raw_audio_data)
+    raw_audio_data = raw_audio_data - np.mean(raw_audio_data)  # zero-centering
 
-    raw_audio_data = raw_audio_data - np.average(raw_audio_data)  # zero-centering
-    freq_magnitude = scipy.fft(raw_audio_data)  # fft computing and normalization
-    freq_magnitude = freq_magnitude[: (n // 2)]  # one side
+    freq_magnitude = fft(raw_audio_data)
+    freq_magnitude = freq_magnitude[: n // 2]
+    freq_magnitude = np.abs(freq_magnitude)
+    freq_magnitude = freq_magnitude / np.sum(freq_magnitude)
 
+    freq_array = np.arange(n // 2) * (sample.frame_rate / n)
     if max_frequency:
         max_index = int(max_frequency * n / sample.frame_rate) + 1
         freq_array = freq_array[:max_index]
         freq_magnitude = freq_magnitude[:max_index]
 
-    freq_magnitude = abs(freq_magnitude)
-    freq_magnitude = freq_magnitude / np.sum(freq_magnitude)
     return freq_array, freq_magnitude
 
 
-def classify_note_attempt_1(freq_array, freq_magnitude):
-    i = np.argmax(freq_magnitude)
-    f = freq_array[i]
-    print("frequency {}".format(f))
-    print("magnitude {}".format(freq_magnitude[i]))
-    return get_note_for_freq(f)
-
-
-def classify_note_attempt_2(freq_array, freq_magnitude):
-    note_counter = Counter()
-    for i in range(len(freq_magnitude)):
-        if freq_magnitude[i] < 0.01:
-            continue
-        note = get_note_for_freq(freq_array[i])
-        if note:
-            note_counter[note] += freq_magnitude[i]
-    return note_counter.most_common(1)[0][0]
-
-
-def classify_note_attempt_3(freq_array, freq_magnitude):
-    min_freq = 82
-    note_counter = Counter()
-    for i in range(len(freq_magnitude)):
-        if freq_magnitude[i] < 0.01:
-            continue
-
-        for freq_multiplier, credit_multiplier in [
-            (1, 1),
-            (1 / 3, 3 / 4),
-            (1 / 5, 1 / 2),
-            (1 / 6, 1 / 2),
-            (1 / 7, 1 / 2),
-        ]:
-            freq = freq_array[i] * freq_multiplier
-            if freq < min_freq:
-                continue
-            note = get_note_for_freq(freq)
-            if note:
-                note_counter[note] += freq_magnitude[i] * credit_multiplier
-
-    return note_counter.most_common(1)[0][0]
-
-
-# If f is within tolerance of a note (measured in cents - 1/100th of a semitone)
-# return that note, otherwise returns None
-# We scale to the 440 octave to check
 def get_note_for_freq(f, tolerance=33):
-    # Calculate the range for each note
+    """
+    Return the closest note to frequency f, or None.
+    Tolerance in cents (1/100 of a semitone)
+    """
     tolerance_multiplier = 2 ** (tolerance / 1200)
-    note_ranges = {
-        k: (v / tolerance_multiplier, v * tolerance_multiplier) for (k, v) in NOTES.items()
-    }
+    note_ranges = {k: (v / tolerance_multiplier, v * tolerance_multiplier) for k, v in NOTES.items()}
 
-    # Get the frequence into the 440 octave
+    # Map f to A4 octave range
     range_min = note_ranges["A"][0]
     range_max = note_ranges["G#"][1]
-    if f < range_min:
-        while f < range_min:
-            f *= 2
-    else:
-        while f > range_max:
-            f /= 2
+    while f < range_min:
+        f *= 2
+    while f > range_max:
+        f /= 2
 
-    # Check if any notes match
-    for (note, note_range) in note_ranges.items():
-        if f > note_range[0] and f < note_range[1]:
+    for note, (low, high) in note_ranges.items():
+        if low <= f <= high:
             return note
     return None
 
 
-# Assumes everything is either natural or sharp, no flats
-# Returns the Levenshtein distance between the actual notes and the predicted notes
-def calculate_distance(predicted, actual):
-    # To make a simple string for distance calculations we make natural notes lower case
-    # and sharp notes cap
-    def transform(note):
-        if "#" in note:
-            return note[0].upper()
-        return note.lower()
+def classify_note_attempt_1(freq_array, freq_magnitude):
+    """Return note with maximum magnitude"""
+    i = np.argmax(freq_magnitude)
+    return get_note_for_freq(freq_array[i])
 
-    return distance(
-        "".join([transform(n) for n in predicted]), "".join([transform(n) for n in actual]),
-    )
+
+def classify_note_attempt_2(freq_array, freq_magnitude):
+    """Weighted by magnitude"""
+    note_counter = Counter()
+    for f, mag in zip(freq_array, freq_magnitude):
+        if mag < 0.01:
+            continue
+        note = get_note_for_freq(f)
+        if note:
+            note_counter[note] += mag
+    if note_counter:
+        return note_counter.most_common(1)[0][0]
+    return None
+
+
+def classify_note_attempt_3(freq_array, freq_magnitude):
+    """Weighted with harmonic multiples"""
+    note_counter = Counter()
+    min_freq = 82  # low E on guitar
+    for f, mag in zip(freq_array, freq_magnitude):
+        if mag < 0.01:
+            continue
+        for mult, credit in [(1, 1), (1 / 3, 0.75), (1 / 5, 0.5), (1 / 6, 0.5), (1 / 7, 0.5)]:
+            freq = f * mult
+            if freq < min_freq:
+                continue
+            note = get_note_for_freq(freq)
+            if note:
+                note_counter[note] += mag * credit
+    if note_counter:
+        return note_counter.most_common(1)[0][0]
+    return None
+
+
+def calculate_distance(predicted, actual):
+    """
+    Levenshtein distance between predicted and actual notes
+    Natural notes lowercase, sharps uppercase
+    """
+    def transform(n):
+        if "#" in n:
+            return n[0].upper()
+        return n.lower()
+
+    pred_str = "".join([transform(n) for n in predicted])
+    actual_str = "".join([transform(n) for n in actual])
+    return distance(pred_str, actual_str)
